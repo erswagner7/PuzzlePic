2b5eafd0abd5c4590227956fde9a53f758a8713e
andrace <acebalao@mail.uc.edu>
2021-06-19 13:29:13 -0400

    Finished Picture.kt file (the DTO) Did some work on the MainViewModel.kt file. Line 16 has an error. I commented it out so it would not break everything. Someone please take a look at that.

--- app/src/main/java/com/example/puzzlepic/dto/Picture.kt
+++ app/src/main/java/com/example/puzzlepic/dto/Picture.kt
@@ -1,5 +1,13 @@
 package com.example.puzzlepic.dto
 
+import com.google.gson.annotations.SerializedName
+
 class Picture {
-    // TODO: implement image storage
+    data class Picture(@SerializedName("id") var pictureID : Int = 0, var pictureTitle : String, var date : String, var location : String, var notes : String,
+                       @SerializedName("id") var puzzleID : Int = 0){
+        override fun toString() : String {
+            return pictureTitle
+            return date
+        }
+    }
 }
