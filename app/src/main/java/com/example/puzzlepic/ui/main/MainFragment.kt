e74047a3a59e0ff3722fdac9c50f0c539907c5eb
salingmw <58449342+salingmw@users.noreply.github.com>
2021-06-20 13:47:28 -0400

    Added ability to take a photo in MainFragment.kt

--- app/src/main/java/com/example/puzzlepic/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/puzzlepic/ui/main/MainFragment.kt
@@ -7,8 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.puzzlepic.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
+
+    private val CAMERA_REQUEST_CODE: Int = 1998
+    val CAMERA_PERMISSION_REQUEST_CODE = 1999
 
     companion object {
         fun newInstance() = MainFragment()
@@ -25,6 +29,56 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        navBarCameraButton.setOnClickListener{
+            prepTakePhoto()
+        }
+    }
+
+    /**
+     * See if there is permission to take photo
+     */
+    private fun prepTakePhoto(){
+        if(ContextCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            takePhoto()
+        } else {
+            val permissionRequest = arrayOf(Manifest.permission.CAMERA)
+            requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    takePhoto()
+                } else {
+                    Toast.makeText(context, "Unable to take photo without permission", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun takePhoto(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+            takePictureIntent -> takePictureIntent.resolveActivity(context!!.packageManager)?.also{
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+        }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            if(requestCode == CAMERA_REQUEST_CODE){
+                val imageBitmap = data!!extras!!.get("data") as Bitmap
+                image1.setImageBitmap(imageBitmap)
+            }
+        }
     }
 
 }
