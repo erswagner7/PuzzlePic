69b7af32fc8ae5658c9b963de3417a6b8a59a1b4
mcquadmj <mmcquad.17@gmail.com>
2021-06-30 16:51:05 -0400

    basic setup for taking pictures and storing them on emulated storage. also setting up to save pictures as puzzles and be displayed onto new fragment when selected.

--- app/src/main/java/com/example/puzzlepic/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/puzzlepic/ui/main/MainFragment.kt
@@ -7,6 +7,9 @@ import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -14,10 +17,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.puzzlepic.R
+import com.example.puzzlepic.dto.Picture
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.main_fragment.*
 import java.io.File
@@ -26,13 +32,14 @@ import java.util.*
 
 class MainFragment : Fragment() {
 
-    private val IMAGE_GALLERY_REQUEST_CODE: Int = 2001
+    private val PUZZLE_GALLERY_REQUEST_CODE: Int = 2001
     private val SAVE_IMAGE_REQUEST_CODE: Int = 1999
     private val CAMERA_REQUEST_CODE: Int = 1998
-
     val CAMERA_PERMISSION_REQUEST_CODE = 1997
 
     private lateinit var currentPhotoPath: String
+    protected var puzzleURI : Uri? = null
+    private var puzzles : ArrayList<Picture> = ArrayList<Picture>()
 
     companion object {
         fun newInstance() = MainFragment()
@@ -58,6 +65,21 @@ class MainFragment : Fragment() {
             var randomURL = viewModel.picture.value?.get(rnds)!!.urls.raw.toString()
             testRandomPhoto(randomURL)
         }
+
+        navBarImageButton.setOnClickListener {
+            prepOpenUserPuzzleGallery()
+        }
+    }
+
+    /*
+     * will request permission for and open users puzzle
+     * gallery by filling in image windows with saved puzzles
+     */
+    private fun prepOpenUserPuzzleGallery() {
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+            type = "image/*"
+            startActivityForResult(this, PUZZLE_GALLERY_REQUEST_CODE)
+        }
     }
 
     /**
@@ -72,7 +94,8 @@ class MainFragment : Fragment() {
         }
     }
 
-    fun onRequestPermissionResult(
+
+        override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
@@ -89,23 +112,61 @@ class MainFragment : Fragment() {
         }
     }
 
+
+    /**
+     * Will take a photo a save it to be later shuffled/randomized into a puzzle
+     */
     private fun takePhoto(){
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
-            takePictureIntent -> takePictureIntent.resolveActivity(requireContext().packageManager)?.also{
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
-        }
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireContext().packageManager)
+            if (takePictureIntent == null) {
+                Toast.makeText(context, "Unable to save puzzle", Toast.LENGTH_LONG).show()
+            } else {
+                val photoFile: File = createImageFile()
+                photoFile?.also {
+                        puzzleURI = FileProvider.getUriForFile(
+                        requireActivity().applicationContext,
+                        "com.puzzlepic.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+                    startActivityForResult(takePictureIntent, SAVE_IMAGE_REQUEST_CODE)
+                }
+            }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK){
-            if(requestCode == CAMERA_REQUEST_CODE){
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
                 val imageBitmap = data!!.extras!!.get("data") as Bitmap
                 image1.setImageBitmap(imageBitmap)
+            } else if (requestCode == SAVE_IMAGE_REQUEST_CODE) {
+                Toast.makeText(context, "Puzzle Saved", Toast.LENGTH_LONG).show()
+                var puzzle = Picture(localUri = puzzleURI.toString())
+                puzzles.add(puzzle)
+            } else if (requestCode == PUZZLE_GALLERY_REQUEST_CODE) {
+                if (data != null && data.data != null) {
+                    val image = data.data
+                    val source =
+                        ImageDecoder.createSource(requireActivity().contentResolver, image!!)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    image1.setImageBitmap(bitmap)
+                }
             }
         }
+    }
 
+    private fun createImageFile() : File {
+        // genererate a unique filename with date.
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        // get access to the directory where we can write pictures.
+        val storageDir:File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("PuzzlePic${timestamp}", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
     }
 
 
@@ -122,15 +183,4 @@ class MainFragment : Fragment() {
             .into(imageView)
     }
 
-
-    //TODO for later use
-    private fun createImageFile() : File {
-        // genererate a unique filename with date.
-        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        // get access to the directory where we can write pictures.
-        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile("puzzlePic${timestamp}", ".jpg", storageDir).apply {
-            currentPhotoPath = absolutePath
-        }
-    }
 }
