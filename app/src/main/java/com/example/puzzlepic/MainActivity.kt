4a0ebf6f6b826eb91106bbcdee8ccbb051ede270
andreace3001 <acebalao@mail.uc.edu>
2021-07-12 19:58:41 -0400

    Commit new branch - Event Details Branch

--- app/src/main/java/com/example/puzzlepic/MainActivity.kt
+++ app/src/main/java/com/example/puzzlepic/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.puzzlepic
 
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import com.example.puzzlepic.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  detector: GestureDetectorCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,74 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+        detector = GestureDetectorCompat(this, DiaryGestureListener())
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+       return if (detector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    inner class DiaryGestureListener: GestureDetector.SimpleOnGestureListener(){
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+
+        override fun onFling(
+            downEvent: MotionEvent?,
+            moveEvent: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?:0.0F
+            var diffY = moveEvent?.x?.minus(downEvent!!.y) ?:0.0F
+
+            return if (Math.abs(diffX) > Math.abs(diffY)){
+                //this is a left or right swipe
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD){
+                    if(diffX> 0){
+                        //this is a right swipe
+                        this@MainActivity.onSwipeRight()
+                    }else {
+                        //left swipe.
+                        this@MainActivity.onLeftSwipe()
+                    }
+                    true
+                }else{
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }else{
+                //this is a bottom or a top swipe.
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD){
+                    if(diffY > 0){
+                        this@MainActivity.onSwipeTop()
+                    }else{
+                        this@MainActivity.onSwipeBottom()
+
+                    }
+                    true
+                    }else{
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+        }
+    }
+//Text in green are subject to change. Which ever makes sense to majority.
+    private fun onSwipeBottom() {
+        Toast.makeText(this, "Bottom Swipe", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeTop() {
+        Toast.makeText(this, "Top Swipe", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onLeftSwipe() {
+        Toast.makeText(this, "Left Swipe", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeRight() {
+        Toast.makeText(this, "Right Swipe", Toast.LENGTH_LONG).show()
     }
 }
