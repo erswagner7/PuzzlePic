5b1d23bf917cb8d30ae50f01ee136440648c4cd5
mcquadmj <mmcquad.17@gmail.com>
2021-07-12 19:37:04 -0400

    Adding functionality and feedback for saving image/puzzle on puzzle_fragment

--- app/src/main/java/com/example/puzzlepic/ui/puzzle/PuzzleFragment.kt
+++ app/src/main/java/com/example/puzzlepic/ui/puzzle/PuzzleFragment.kt
@@ -1,22 +1,33 @@
 @file:Suppress("DEPRECATION")
 package com.example.puzzlepic.ui.puzzle
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.puzzlepic.R
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.puzzle_fragment.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class PuzzleFragment : Fragment() {
 
     lateinit var randomUrl: String
     lateinit var navController: NavController
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -51,6 +62,35 @@ class PuzzleFragment : Fragment() {
             navController!!.navigate(R.id.action_puzzleFragment_to_mainFragment)
         }
 
+
+        save_puzzle_button.setOnClickListener{
+            val drawable = bigPicture.drawable
+            val bitmap = (drawable as BitmapDrawable).bitmap
+            val uri:Uri = storeImage(bitmap)
+            bigPicture.isVisible = false
+            Save_Success_Text.isVisible = true
+            Save_Success_Text.text = "saved: $uri"
+        }
+    }
+
+    private fun storeImage(image : Bitmap): Uri {
+
+        // Create a file to save the image
+        val file = createImageFile()
+
+            // Get the file output stream
+            val stream: OutputStream = FileOutputStream(file)
+
+            // Compress the bitmap
+            image.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+            // Flush the output stream
+            stream.flush()
+
+            // Close the output stream
+            stream.close()
+        // Return the saved image path to uri
+        return Uri.parse(file.absolutePath)
     }
 
     /**
@@ -67,6 +107,16 @@ class PuzzleFragment : Fragment() {
             .load(Image_Url)
             .resize(320,450)
             .into(imageView)
+    }
+
+    private fun createImageFile() : File {
+        // genererate a unique filename with date.
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        // get access to the directory where we can write pictures.
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("PuzzlePic${timestamp}", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
     }
 
 }
