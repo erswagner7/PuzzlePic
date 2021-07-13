ae4e3778ef5d4a89be2e4a632b63a925de3f4340
dumforms <84052826+dumforms@users.noreply.github.com>
2021-07-12 21:47:40 -0400

    implemented binding to replaced depreciated synthetics

--- app/src/main/java/com/example/puzzlepic/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/puzzlepic/ui/main/MainFragment.kt
@@ -27,7 +27,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.puzzlepic.R
 import com.example.puzzlepic.dto.Picture
-import kotlinx.android.synthetic.main.main_fragment.*
+import com.example.puzzlepic.databinding.MainFragmentBinding
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -45,6 +45,9 @@ class MainFragment : Fragment() {
     protected var puzzleURI : Uri? = null
     private var puzzles : ArrayList<Picture> = ArrayList<Picture>()
 
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -53,7 +56,9 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -65,11 +70,11 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
-        navBarCameraButton.setOnClickListener{
+        binding.navBarCameraButton.setOnClickListener{
             prepTakePhoto()
         }
 
-        randomPuzzleButton.setOnClickListener {
+        binding.randomPuzzleButton.setOnClickListener {
             val rnds = (0..viewModel.picture.value!!.size).random() // generated random from 0 to size of current photo array included
             var randomURL = viewModel.picture.value?.get(rnds)!!.urls.raw.toString()
             if(randomURL != null) {
@@ -84,7 +89,7 @@ class MainFragment : Fragment() {
             }
         }
 
-        navBarImageButton.setOnClickListener {
+        binding.navBarImageButton.setOnClickListener {
             prepOpenUserPuzzleGallery()
         }
 
@@ -161,7 +166,7 @@ class MainFragment : Fragment() {
         if (resultCode == RESULT_OK) {
             if (requestCode == CAMERA_REQUEST_CODE) {
                 val imageBitmap = data!!.extras!!.get("data") as Bitmap
-                image1.setImageBitmap(imageBitmap)
+                binding.image1.setImageBitmap(imageBitmap)
             } else if (requestCode == SAVE_IMAGE_REQUEST_CODE) {
                 Toast.makeText(context, "Puzzle Saved", Toast.LENGTH_LONG).show()
                 var puzzle = Picture(localUri = puzzleURI.toString())
@@ -172,7 +177,7 @@ class MainFragment : Fragment() {
                     val source =
                         ImageDecoder.createSource(requireActivity().contentResolver, image!!)
                     val bitmap = ImageDecoder.decodeBitmap(source)
-                    image1.setImageBitmap(bitmap)
+                    binding.image1.setImageBitmap(bitmap)
                 }
             }
         }
@@ -188,5 +193,9 @@ class MainFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
