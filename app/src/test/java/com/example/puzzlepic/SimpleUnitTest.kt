1d75f5e68107c9f1b19b4755d87c07a65ebd2f11
dumforms <84052826+dumforms@users.noreply.github.com>
2021-07-12 21:56:41 -0400

    added 2 unit tests

--- app/src/test/java/com/example/puzzlepic/SimpleUnitTest.kt
+++ app/src/test/java/com/example/puzzlepic/SimpleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.example.puzzlepic
 
+import com.example.puzzlepic.dto.Picture
+import com.example.puzzlepic.dto.UnsplashPhotoUrls
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -15,5 +17,26 @@ class SimpleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun confirmPicture_outputsPicture() {
+        confirmPhotoURL_returnsPhotoURL()
+        val testURL = UnsplashPhotoUrls()
+        val picture = Picture(
+            "testID",
+            "testTitle",
+            "Jan 1 1970",
+            "testLocation",
+            "testNotes",
+            123, testURL)
+        assertEquals("testTitle: Jan 1 1970", picture.toString())
+    }
+
+    @Test
+    fun confirmPhotoURL_returnsPhotoURL() {
+        val testURL = UnsplashPhotoUrls()
+        assert(testURL !== null)
+        assert(testURL is UnsplashPhotoUrls)
     }
 }
