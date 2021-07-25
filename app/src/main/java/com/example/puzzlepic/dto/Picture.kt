ac3839d3045fe1eba61e514f3262e15f99b53aa7
Erich Wagner <erswagner@gmail.com>
2021-07-25 12:10:24 -0500

    Raj's Code Review Suggested Commits

--- app/src/main/java/com/example/puzzlepic/dto/Picture.kt
+++ app/src/main/java/com/example/puzzlepic/dto/Picture.kt
@@ -8,9 +8,7 @@ import kotlinx.android.parcel.Parcelize
 data class Picture(@SerializedName("id") var pictureID : String = "",
                    @SerializedName("alt_description") var pictureTitle : String = "",
                    @SerializedName("created_at") var date : String = "",
-                   var location : String = "",
-                   var notes : String = "",
-                   var puzzleID : Int = 0,
+                   var location : String = "", var notes : String = "", var puzzleID : Int = 0,
                    @SerializedName("urls") var urls : UnsplashPhotoUrls = UnsplashPhotoUrls(),
                    var localUri : String = "" ) {
         override fun toString() : String {
