48ba26c2e617bdd62bd35192d2b53608c2318957
Anthony Morgan <morganaj@ucmail.uc.edu>
2021-07-25 18:04:10 -0400

    removed commented and unused function getPictureByName() from interface IPictureDAO

--- app/src/main/java/com/example/puzzlepic/dao/IPictureDAO.kt
+++ app/src/main/java/com/example/puzzlepic/dao/IPictureDAO.kt
@@ -11,7 +11,4 @@ interface IPictureDAO {
     @GET("/photos?client_id=gNYmyVdZ2_qHp3L4i4yzrulyHU70MdvnmsFFyB_5y38&per_page=100")
     fun getRandomImage(): Call<ArrayList<Picture>>
 
-    //
-    //@GET("")
-    //fun getPictureByName(pictureName:String) : Call<ArrayList<Picture>> //going to return a specific picture.
 }
